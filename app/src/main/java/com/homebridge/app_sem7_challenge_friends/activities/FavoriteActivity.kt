a3745117f69d71f34119828e4f695d313b18b55f
Camila Amaro <camilaamarov@yahoo.es>
2024-09-29 22:47:12 -0500

    feat: Add funcionality to Favorite and Main Activities

--- app/src/main/java/com/homebridge/app_sem7_challenge_friends/activities/FavoriteActivity.kt
+++ app/src/main/java/com/homebridge/app_sem7_challenge_friends/activities/FavoriteActivity.kt
@@ -1,21 +1,60 @@
 package com.homebridge.app_sem7_challenge_friends.activities
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.homebridge.app_sem7_challenge_friends.R
+import com.homebridge.app_sem7_challenge_friends.adapters.PersonAdapter
+import com.homebridge.app_sem7_challenge_friends.db.AppDatabase
+import com.homebridge.app_sem7_challenge_friends.models.Person
 
-class FavoriteActivity : AppCompatActivity() {
+class FavoriteActivity : AppCompatActivity(), PersonAdapter.OnItemClickListener {
+    private lateinit var rvFavorite : RecyclerView
+
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_favorite)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        setSupportActionBar(findViewById(R.id.toolbar2))
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setDisplayShowHomeEnabled(true)
+        rvFavorite = findViewById(R.id.rvFavorite)
+    }
+
+    override fun onResume() {
+        super .onResume()
+
+        loadPeople { people ->
+            rvFavorite.adapter = PersonAdapter(people, this)
+            rvFavorite.layoutManager = LinearLayoutManager(this@FavoriteActivity)
+        }
+    }
+
+    private fun loadPeople(onComplete: (List<Person>)-> Unit) {
+        val dao = AppDatabase.getInstance(this).getDao()
+
+        onComplete(dao.getAll())
+    }
+
+    override fun onItemClick(person: Person) {
+        val dao = AppDatabase.getInstance(this).getDao()
+
+        dao.delete(person)
+
+        Toast.makeText(this, "Person  "+person.firstName+" deleted from favorites", Toast.LENGTH_SHORT).show()
+
+        loadPeople { people ->
+            rvFavorite.adapter = PersonAdapter(people, this)
+            rvFavorite.layoutManager = LinearLayoutManager(this@FavoriteActivity)
         }
     }
 }
--- app/src/main/java/com/homebridge/app_sem7_challenge_friends/activities/MainActivity.kt
+++ app/src/main/java/com/homebridge/app_sem7_challenge_friends/activities/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.homebridge.app_sem7_challenge_friends.activities
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -8,14 +11,25 @@ import androidx.core.view.WindowInsetsCompat
 import com.homebridge.app_sem7_challenge_friends.R
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var peopleBtn: Button
+    private lateinit var favoriteBtn: Button
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        peopleBtn = findViewById(R.id.btPeople)
+        favoriteBtn = findViewById(R.id.btFavorite)
+
+        peopleBtn.setOnClickListener {
+            val intent = Intent(this, PeopleActivity::class.java)
+            startActivity(intent)
+        }
+
+        favoriteBtn.setOnClickListener {
+            val intent = Intent(this, FavoriteActivity::class.java)
+            startActivity(intent)
         }
     }
 }
