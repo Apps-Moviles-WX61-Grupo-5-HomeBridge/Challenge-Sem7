3b53b39956090c203080d3d834f5ffafa08a0ae2
Camila Amaro <camilaamarov@yahoo.es>
2024-09-29 19:00:41 -0500

    feat: Add components to activity_main layout

--- app/src/main/java/com/homebridge/app_sem7_challenge_friends/activities/MainActivity.kt
+++ app/src/main/java/com/homebridge/app_sem7_challenge_friends/activities/MainActivity.kt
@@ -1,10 +1,11 @@
-package com.homebridge.app_sem7_challenge_friends
+package com.homebridge.app_sem7_challenge_friends.activities
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.homebridge.app_sem7_challenge_friends.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
